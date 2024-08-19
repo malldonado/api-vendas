78ac54b06af3787a6079aacb9756a7ceebf01595
malldonado <matheussilvacontat@gmail.com>
2024-08-19 17:02:59 -0300

    update project

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,17 +2,11 @@ import { Router } from 'express';
 import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
-import passwordRouter from '@modules/users/routes/password.routes';
-import profileRouter from '@modules/users/routes/profile.routes';
-import customersRouter from '@modules/customers/routes/customers.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
-routes.use('/password', passwordRouter);
-routes.use('/profile', profileRouter);
-routes.use('/customers', customersRouter);
 
 export default routes;
